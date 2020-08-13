15ffbca0a3803108fe9ba059c0ac455e55cecf05
Nikolay Kornev <nikolay-kornev@users.noreply.github.com>
2020-08-13 23:34:19 +0000

    Apply fixes from StyleCI

--- src/Command/CalculateMetricCommand.php
+++ src/Command/CalculateMetricCommand.php
@@ -8,11 +8,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CalculateMetricCommand extends Command
 {
-    private const DATA_FILE         = '/../../data/Jira.csv';
-    private const COLUMN_LOGGED     = 'Log Work';
-    private const COLUMN_ESTIMATE   = 'Original Estimate';
+    private const DATA_FILE = '/../../data/Jira.csv';
+    private const COLUMN_LOGGED = 'Log Work';
+    private const COLUMN_ESTIMATE = 'Original Estimate';
     private const COLUMN_WORK_RATIO = 'Work Ratio';
-    private const COLUMN_ISSUE_KEY  = 'Issue key';
+    private const COLUMN_ISSUE_KEY = 'Issue key';
 
     protected static $defaultName = 'app:generate-metric';
 
@@ -20,29 +20,27 @@ class CalculateMetricCommand extends Command
     {
         $this
             ->setDescription('Generates Estimation Accuracy Metric for L10')
-            ->setHelp('This command generates estimation accuracy metric for L10 based on CSV file exported from Jira.')
-        ;
+            ->setHelp('This command generates estimation accuracy metric for L10 based on CSV file exported from Jira.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $filePath = __DIR__ . self::DATA_FILE;
-        $handle   = fopen($filePath, 'rb');
+        $filePath = __DIR__.self::DATA_FILE;
+        $handle = fopen($filePath, 'rb');
         if (false === $handle) {
-            $output->writeln('Failed to open data file at "' . self::DATA_FILE . '".');
+            $output->writeln('Failed to open data file at "'.self::DATA_FILE.'".');
             exit();
         }
 
-        $rowCount              = 0;
+        $rowCount = 0;
         $columnsWithTimeLogged = [];
-        $columnWithEstimate    = null;
-        $columnWithWorkRatio   = null;
-        $columnWithIssueKey    = null;
-        $outliers              = [];
-        $sumOfTimeLogged       = 0;
-        $sumOfEstimates        = 0;
+        $columnWithEstimate = null;
+        $columnWithWorkRatio = null;
+        $columnWithIssueKey = null;
+        $outliers = [];
+        $sumOfTimeLogged = 0;
+        $sumOfEstimates = 0;
         while (($row = fgetcsv($handle)) !== false) {
-
             if ($rowCount === 0) {
                 foreach ($row as $index => $cell) {
                     if ($cell === self::COLUMN_LOGGED) {
@@ -56,17 +54,17 @@ class CalculateMetricCommand extends Command
                     }
                 }
                 if (empty($columnsWithTimeLogged)) {
-                    $output->writeln('No "' . self::COLUMN_LOGGED . '" columns found.');
+                    $output->writeln('No "'.self::COLUMN_LOGGED.'" columns found.');
                     exit();
                 }
 
                 if (null === $columnWithEstimate) {
-                    $output->writeln('No "' . self::COLUMN_ESTIMATE . '" column found.');
+                    $output->writeln('No "'.self::COLUMN_ESTIMATE.'" column found.');
                     exit();
                 }
 
                 if (null === $columnWithWorkRatio) {
-                    $output->writeln('No "' . self::COLUMN_WORK_RATIO . '" column found.');
+                    $output->writeln('No "'.self::COLUMN_WORK_RATIO.'" column found.');
                     exit();
                 }
             } else {
@@ -76,13 +74,13 @@ class CalculateMetricCommand extends Command
                         $issueKey = $cell;
                     }
 
-                    if (!empty($cell) && in_array($index, $columnsWithTimeLogged, true)) {
+                    if (! empty($cell) && in_array($index, $columnsWithTimeLogged, true)) {
                         [, , , $time] = explode(';', $cell);
-                        $sumOfTimeLogged += (int)$time;
+                        $sumOfTimeLogged += (int) $time;
                     }
 
                     if ($index === $columnWithEstimate) {
-                        $sumOfEstimates += (int)$cell;
+                        $sumOfEstimates += (int) $cell;
                     }
 
                     if ($index === $columnWithWorkRatio && strlen($cell) > 4) {
@@ -94,13 +92,13 @@ class CalculateMetricCommand extends Command
             $rowCount++;
         }
 
-        $output->writeln('TOTAL ISSUES:   ' . ($rowCount-1));
-        $output->writeln('TOTAL ESTIMATE: ' . $sumOfEstimates);
-        $output->writeln('TOTAL LOGGED:   ' . $sumOfTimeLogged);
+        $output->writeln('TOTAL ISSUES:   '.($rowCount - 1));
+        $output->writeln('TOTAL ESTIMATE: '.$sumOfEstimates);
+        $output->writeln('TOTAL LOGGED:   '.$sumOfTimeLogged);
 
         $rate = round(($sumOfEstimates / $sumOfTimeLogged - 1) * 100, 2);
 
-        $output->writeln('RATE (est/log): ' . $rate . '%');
+        $output->writeln('RATE (est/log): '.$rate.'%');
 
         if ($outliers) {
             $output->writeln("\n-----------------------------------------\n");
